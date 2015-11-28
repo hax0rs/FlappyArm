aea7110049f84113a91ce652f240bb383914a648
JoshuaRiddell <joshua.riddell11@gmail.com>
2015-11-28 12:17:34 +1000

    Created client class to open connection
    Gets user arguments and compares with the stored connection info.
    Overwrites old connection info if new is specified.

--- rpi/main.py
+++ rpi/main.py
@@ -1,19 +1,18 @@
 import argparse
+import net_com
 # import pigpio as pg
 
 
 def main():
-    update_network_settings()
+    usr_args = get_usr_args()
+    net_client = net_com.Client(usr_args)
 
 
-def update_network_settings():
+def get_usr_args():
     parser = argparse.ArgumentParser(description='Begin the arm controller')
-    parser.add_argument('ip', help='the ip of the host computer')
-    parser.add_argument('port', help='the ip of the host computer')
-    network = parser.parse_args()
-
-    print(network.ip)
-    print(network.port)
+    parser.add_argument('-ip', help='the ip of the host computer')
+    parser.add_argument('-port', help='the ip of the host computer')
+    return parser.parse_args()
 
 if __name__ == '__main__':
     main()
