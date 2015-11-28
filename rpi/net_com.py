b48f4dc477cbec3d958f1291807c907c7fa6bcc0
JoshuaRiddell <joshua.riddell11@gmail.com>
2015-11-28 14:53:10 +1000

    Network comms initiates and holds connection

--- rpi/net_com.py
+++ rpi/net_com.py
@@ -4,6 +4,22 @@ from time import sleep
 
 NETWORK_CONFIG = "network_config"
 
+
+class NetworkThread(threading.Thread):
+    def __init__(self, parent):
+        super().__init__()
+        self.daemon = True
+
+        self.lock = parent.lock
+
+    def run(self):
+        print("Network connection loop initiated on auxiliary thread")
+
+        while True:
+            if self.exit:
+                break
+
+
 class Client(object):
     def __init__(self, usr_args):
         net_settings = self.update_net_settings(usr_args)
@@ -17,18 +33,28 @@ class Client(object):
                 sleep(1)
                 print("Trying again...")
 
+        while True:
+            print(self.sock.recv(1024).decode('utf-8'))
+
+    def begin_command_loop(self):
+        self.lock = threading.Lock()
+        self.network_thread = NetworkThread(self)
+
     def update_net_settings(self, usr_args):
-        fd = open(NETWORK_CONFIG)
         try:
+            fd = open(NETWORK_CONFIG)
             ip = fd.readline()
             port = int(fd.readline())
         except:
             ip, port = None, None
-        fd.close()
+        try:
+            fd.close()
+        except:
+            pass
         if usr_args.ip is not None:
             ip = usr_args.ip
         if usr_args.port is not None:
-            port = usr_args.port
+            port = int(usr_args.port)
 
         if ip is None or port is None:
             raise Exception("Please specify a valid IP and port when running the script")
