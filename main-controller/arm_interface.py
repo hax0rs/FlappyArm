402dfdc60027f235270cf61cca143289ff9b5187
JoshuaRiddell <joshua.riddell11@gmail.com>
2015-11-28 16:33:32 +1000

    Added multithreaded networking basics

--- main-controller/arm_interface.py
+++ main-controller/arm_interface.py
@@ -3,24 +3,61 @@ import socket as sk
 import os
 
 ARM_CONFIG = "arm_config"
+HOST = ""
+PORT = "50007"
 
 
 class ArmInterface(object):
     def __init__(self):
-        self.serv_lock = threading.Lock()
         self.server = ServerThread(self)
-        self.kin_lock = threading.Lock()
         self.kin = KinematicsThread(self)
+
+        self.position = [0 for x in range(len(self.kin.load_arm_config()))]
+
+    def begin_connection():
+        self.server.start()
+
+    def set_servo(num, percentage):
+        self.server.lock.acquire()
+        self.server.queue.append((num, percentage))
+        self.server.lock.release()
+
+    def reset_arm():
+        pass
 
 
 class ServerThread(threading.Thread):
     def __init__(self, parent):
         super().__init__()
         self.parent = parent
-        self.lock = parent.serv_lock
+        self.lock = threading.Lock()
+
+        self.sock = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+        self.sock.bind((HOST, PORT))
+
+        self.queue = []  # public queue, need lock to access this
+        self._queue = []  # private queue, only accessed inside thread
 
     def run(self):
-        print("kin thread ran")
+        self.sock.listen(1)
+        print("Attempting connection to pi on separate thread...")
+        self.conn, addr = self.sock.accept()
+        print("Connected by", addr)
+        data = self.conn.recv(1024)
+        print(data.decode('utf-8'))
+
+        while True:
+            self.lock.acquire()
+            if self.queue != []:
+                self._queue.extend(self.queue)
+            self.queue = []
+            self.lock.release()
+
+            for message in self._queue:
+                print(message)  # send the message
+
+        self.sock.close()
+        print("pi thread exit")
 
 
 class KinematicsThread(threading.Thread):
@@ -29,7 +66,8 @@ class KinematicsThread(threading.Thread):
     def __init__(self, parent):
         super().__init__()
         self.parent = parent
-        self.lock = parent.kin_lock
+        self.lock = threading.Lock()
+        self.servo_poi = self.load_arm_config()
 
     def run(self):
         print("kin thread ran")
