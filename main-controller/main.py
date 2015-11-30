d1ae6c1eb341259c6da9a9a087fcecac5e85228e
Dragan Marjanovic - MBP <gagalug13@gmail.com>
2015-12-01 00:28:36 +1000

    /play/<int>
    Changed the URL for playing the game to /play/<int> rather than the
    root URL to allow for a proper home page.

--- main-controller/main.py
+++ main-controller/main.py
@@ -16,9 +16,29 @@ arm.begin_connection()
 
 
 @app.route("/")
-@app.route("/<int:controllerID>")
+def home():
+    return(render_template('home.html'))
+
+
+@app.route("/play/<int:controllerID>")
 def root(controllerID=None):
-    return (render_template('control.html', ip=HOST_IP, controllerID=controllerID))
+    controller_name = {
+        1 : "Base",
+        2 : "Shoulder",
+        3 : "Elbow",
+        4 : "Wrist Rotation",
+        5 : "Wrist",
+        6 : "Pincer Rotation",
+        7 : "Pincer"
+    }
+    if (controllerID in controller_name):
+        controller_name = str(controller_name[controllerID])
+    else:
+        controller_name = None
+    return (render_template('control.html',
+                            ip=HOST_IP,
+                            controllerID=controllerID,
+                            controller_name=controller_name))
 
 
 @socketio.on('json')
@@ -34,7 +54,7 @@ def handle_json(json):
     print ("The value for this motor is:  " + str(json['value']))
 
 
-@app.route("/leaderboard/")
+@app.route("/leaderboard")
 def leaderboard():
     return ("<ol><li>UQ hax0rs</li></ol>")
 
