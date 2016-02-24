eae8cca1fcb4d88eaf1d6de49751fb7edcf0a759
Dragan Marjanovic - Air <dmarj97@gmail.com>
2016-02-25 01:10:05 +1000

    Webhook wigs restart

--- hooks/main.py
+++ hooks/main.py
@@ -8,30 +8,36 @@ base_path = "/home/gagalug/"
 projects = ["FlappyArm"]
 
 
+
+def git_pull_in_dir(base_path, service):
+    """ """
+    previous_cwd = os.getcwd()
+    try:
+        os.chdir(base_path + service)
+        try:
+            out = sp.check_output(["git", "pull"], timeout=120).decode('utf-8')
+            code = 0
+        except sp.CalledProcessError as e:
+            out = e.output.decode('utf-8')
+            out += "\nErrored out with code " + str(e.returncode) + "."
+            code = e.returncode
+        out += "\n"
+        os.chdir(previous_cwd)
+    except:
+        out = "Could not change directory."
+        code = -1
+    return out, code
+
+def service_restart():
+    os.system('sudo /home/gagalug/flappyarm_restart.sh')
+    return 1
+
+
 @app.route("/", methods=['POST'])
 def hook():
-    # return ("Hooked me right in the gabber")
-    git_pull_in_dir(base_path, service)
+    data = git_pull_in_dir(base_path, projects[0])
+    restart = service_restart()
+    return ("Completed")
 
 if __name__ == "__main__":
     app.run('0.0.0.0')
-
-
-
-def git_pull_in_dir(base_path, service):
-    """
-    A service name should match with both its name in supervisor and its /srv/*
-    path.
-    """
-    previous_cwd = os.getcwd()
-    os.chdir(base_path + service)
-    try:
-        out = sp.check_output(["git", "pull"], timeout=120).decode('utf-8')
-        code = 0
-    except sp.CalledProcessError as e:
-        out = e.output.decode('utf-8')
-        out += "\nErrored out with code " + str(e.returncode) + "."
-        code = e.returncode
-    out += "\n"
-    os.chdir(previous_cwd)
-    return out, code
